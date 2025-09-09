39e2a2ab44cff89bb5c1e5aace858fba16d18b16
MD.Rakibul Islam <mdraislam56@gmail.com>
2025-09-10 01:56:56 +0530

    update the code

--- js/login.js
+++ js/login.js
@@ -2,11 +2,16 @@
 
 
 
-document.getElementById('login-btn').addEventListener('click',function (event){
-     event.preventDefault();
-    console.log('btn clicked');
-
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault();
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
-    
+    const pinNumber = document.getElementById("pin-number").value;
+    // validation ///
+    if (phoneNumber === "45" && pinNumber === "1234") {
+        console.log("ok your are a login");
+        window.location.href ="/home.html"
+    } else {
+        alert('please give a valid number and pin');
+    }
+
 })
