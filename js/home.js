062b9fda7947b726b852b9c6472524b1da5bfa24
MD.Rakibul Islam <mdraislam56@gmail.com>
2025-09-10 19:36:34 +0530

    add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('home');
+// step-1 : add event hamdler 
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        // step-2 : how to add money
+
+        const addMoney = document.getElementById('amount').value;
+        console.log(addMoney);
+
+        // get the pin number 
+        const pinNumbers = document.getElementById('pin').value;
+        console.log(pinNumbers);
+        
+
+
+    })
